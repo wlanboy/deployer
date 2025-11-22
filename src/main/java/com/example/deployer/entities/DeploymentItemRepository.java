bb828f519d7a4ca6dcf9ea2bc88cffce3789aa00
wlanboy <wlanboy@github.com>
2025-11-22 10:56:42 +0100

    Add methods to DeploymentItemRepository and DeploymentRepository for enhanced querying

--- src/main/java/com/example/deployer/entities/DeploymentItemRepository.java
+++ src/main/java/com/example/deployer/entities/DeploymentItemRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeploymentItemRepository extends JpaRepository<DeploymentItem, Long> {
     List<DeploymentItem> findByDeploymentId(String deploymentId);
+
+    List<DeploymentItem> findByDeploymentIdAndRepoId(String deploymentId, String repoId);
 }
 
--- src/main/java/com/example/deployer/entities/DeploymentRepository.java
+++ src/main/java/com/example/deployer/entities/DeploymentRepository.java
@@ -1,5 +1,14 @@
 package com.example.deployer.entities;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DeploymentRepository extends JpaRepository<Deployment, String> {}
+public interface DeploymentRepository extends JpaRepository<Deployment, String> {
+
+    List<Deployment> findByRepoId(String repoId);
+
+    List<Deployment> findByName(String name);
+
+    boolean existsByIdAndRepoId(String id, String repoId);
+}
