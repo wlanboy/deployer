74be861a1bbc0f69a850d357c096ed4890a7c1db
wlanboy <wlanboy@github.com>
2025-11-23 20:46:24 +0100

    Enable scheduling support in DeployerApplication for enhanced task management

--- src/main/java/com/example/deployer/DeployerApplication.java
+++ src/main/java/com/example/deployer/DeployerApplication.java
@@ -2,8 +2,10 @@ package com.example.deployer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DeployerApplication {
 
 	public static void main(String[] args) {
