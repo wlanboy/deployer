dcfe817f4bb7a8d980e9f50a55e4836b35ff5d3a
wlanboy <wlanboy@github.com>
2026-04-08 06:18:41 +0200

    removed public

--- src/main/java/com/example/deployer/configuration/OpenApiConfig.java
+++ src/main/java/com/example/deployer/configuration/OpenApiConfig.java
@@ -10,7 +10,7 @@ import io.swagger.v3.oas.models.info.Info;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    OpenAPI customOpenAPI() {
         return new OpenAPI()
             .info(new Info()
                 .title("Deployment API")
